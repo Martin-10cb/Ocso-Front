80fdfd2023ad3a0855478c5796091bbb71c3743b
Martin-10cb <martincalzadillab@outlook.com>
2024-10-18 14:57:10 -0600

    feat: Cookies in SSC

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,38 +1,73 @@
-'use client';
+"use client";
 import { API_URL } from "@/constants";
-import { Button, Input, Link } from "@nextui-org/react";
+import { Button, Input, Link, Spinner, spinner } from "@nextui-org/react";
 import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-export default function LoginPage(){
-
-    const handleSubmit = async(e: React.FormEvent) => {
-        e.preventDefault ()
-        const formData = new FormData(e.target)
-        let authData: any = {}
-        authData.userEmail = formData.get("userEmail")
-        authData.userPassword = formData.get("userPassword")
-        const { data } = await axios.post(`${API_URL}/auth/login`, {
-            ...authData
-        }, {
-            withCredentials: true,
-        });
-        return;
+export default function LoginPage() {
+  const [submitting, setSubmitting] = useState(false);
+  const router = useRouter();
+  const handleSubmit = async (e: React.FormEvent) => {
+    setSubmitting(true);
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    let authData: any = {};
+    authData.userEmail = formData.get("userEmail");
+    authData.userPassword = formData.get("userPassword");
+    try {
+      const response = await axios.post(
+        `${API_URL}/auth/login`,
+        {
+          ...authData,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      if (response.status === 201) router.push("/dashboard");
+      setSubmitting(false);
+    } catch (e) {
+      setSubmitting(false);
     }
+    return;
+  };
 
-    return (
-        <form className="bg-orange-500 px-10 py-2 rounded-md" onSubmit={handleSubmit}>
-            <p className="text-2xl my-4 text-white">Iniciar sesion <span></span></p>
-            <div className="flex flex-col gap-2 my-4 items-center">
-                <Input label="Email" name="userEmail" type="email" isRequired={true} size="sm"/>
-                <Input label="Contraseña" name="userPassword" type="password" isRequired={true} size="sm"/>
-            </div>
-            <div className=" flex flex-col items-center gap-2">
-                <Button color="primary" type="submit">Iniciar Sesion</Button>
-                <p className="text-white">
-                    ¿No tienes una cuenta? 
-                    <Link href="/signup" className="text-red-600 underline">Registrate</Link>
-                </p>
-            </div>
-        </form>
-    )
+  return (
+    <form
+      className="bg-orange-500 px-10 py-2 rounded-md"
+      onSubmit={handleSubmit}
+    >
+      <p className="text-2xl my-4 text-white">
+        Iniciar sesion <span></span>
+      </p>
+      <div className="flex flex-col gap-2 my-4 items-center">
+        <Input
+          label="Email"
+          name="userEmail"
+          type="email"
+          isRequired={true}
+          size="sm"
+        />
+        <Input
+          label="Contraseña"
+          name="userPassword"
+          type="password"
+          isRequired={true}
+          size="sm"
+        />
+      </div>
+      <div className=" flex flex-col items-center gap-2">
+        <Button color="primary" type="submit" disabled={submitting}>
+          {submitting ? "Enviando..." : "Iniciar Sesion"}
+        </Button>
+        <p className="text-white">
+          ¿No tienes una cuenta?
+          <Link href="/signup" className="text-red-600 underline">
+            Registrate
+          </Link>
+        </p>
+      </div>
+    </form>
+  );
 }
