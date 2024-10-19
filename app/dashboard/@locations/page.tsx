35bdc0b117bf88cc61e81c06be546c60f9cb452e
Martin-10cb <martincalzadillab@outlook.com>
2024-10-19 12:59:49 -0600

    Implemented LocationCards and link to managers

--- app/dashboard/@locations/page.tsx
+++ app/dashboard/@locations/page.tsx
@@ -3,6 +3,7 @@ import { Location } from "@/entities";
 import axios from "axios";
 import { cookies } from "next/headers";
 import SelectLocation from "./_components/SelectLocation";
+import LocationCards from "./_components/LocationCard";
 
 const LocationsPage = async ({
   searchParams,
@@ -33,6 +34,9 @@ const LocationsPage = async ({
       <div className="w-full flex flex-col items-center h-[90vh]">
         <div className="w-1/2 my-10">
           <SelectLocation locations={data} store={searchParams?.store} />
+        </div>
+        <div className="w-8/12">
+          <LocationCards store={searchParams.store} />
         </div>
       </div>
     </div>
---
+++ app/dashboard/error.tsx
@@ -0,0 +1,4 @@
+'use client'
+export default function ErrroMainDashboard () {
+    return "Ocurrio un error";
+}
