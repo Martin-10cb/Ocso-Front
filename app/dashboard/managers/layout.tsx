d5fbc9ab0e1ee399156f05d087b386e0503c24cc
Martin-10cb <martincalzadillab@outlook.com>
2024-10-31 14:00:42 -0600

    feat: New manager route config

--- app/dashboard/managers/layout.tsx
+++ app/dashboard/managers/layout.tsx
@@ -1,13 +1,21 @@
 import { ReactNode } from "react";
 import ManagerCards from "./_components/ManagerCard";
 
-export default function LayoutManager({ children }: { children: ReactNode }) {
+export default function LayoutManager({
+  children,
+  count,
+}: {
+  children: ReactNode;
+  count: ReactNode;
+}) {
   return (
     <>
       <div className="w-4/12 max-h-[90vh] h-[90vh] overflow-hidden overflow-y-auto">
         <ManagerCards />
       </div>
-      <div>{children}</div>
+      <div className="w-7/12 flex flex-col justify-center items-center gap-10">
+        <div>{children}</div> <div>{count}</div>
+      </div>
     </>
   );
 }
