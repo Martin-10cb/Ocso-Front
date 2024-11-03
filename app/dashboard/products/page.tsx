08b52a789a6de198c1c6b91fd27bb7f8b6070705
Martin-10cb <martincalzadillab@outlook.com>
2024-11-02 22:36:45 -0600

    Busqueda

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,5 +1,27 @@
-export const ProductsPage = () => {
-    return null
-}
+import { API_URL } from "@/constants";
+import { Product } from "@/entities";
+import { authHeaders } from "@/helpers/authHeaders";
+import ProductCard from "./_components/ProductCard";
+import Link from "next/link";
+import FilteredCards from "./_components/FilteredCards";
+
+export const ProductsPage = async () => {
+  const response = await fetch(`${API_URL}/products`, {
+    headers: {
+      ...authHeaders(),
+    },
+    next: {
+      tags: ["dashboard:products"],
+    },
+  });
+  const products: Product[] = await response.json();
+  return (
+    <div className="h-[90vh] w-full">
+      <div className="w-3/12">
+        <FilteredCards products={products} />
+      </div>
+    </div>
+  );
+};
 
 export default ProductsPage;
